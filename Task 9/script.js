3d54c95f6b1a71afdbd0c2adbd836a0a06ee9cd6
Papalucis <papalucis@gmail.com>
2022-05-15 11:30:49 +0300

    9th completed

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,24 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor (title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive() {
+        return this.budget > 100;
+    }
+    
+}
+
+const movie1 = new Movie ('Godfather', 'Francis Coppola', 6);
+const movie2 = new Movie ('Doctor Strange', 'Scott Derrickson', 165)
+const movie3 = new Movie ('Road to Perdition', 'Sam Mendes', 80);
+
+console.log(movie1.wasExpensive());
+console.log(movie2.wasExpensive());
+console.log(movie3.wasExpensive());
